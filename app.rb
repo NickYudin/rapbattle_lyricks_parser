6e6a65141508d9224629b40dab477dbbff038443
Vseotets <metalworldowner@gmail.com>
2021-10-08 08:42:04 +0300

    get links for lyrics in arr

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'mechanize'
+require 'pry'
 
 agent = Mechanize.new
-page = agent.get('https://genius.com/albums/King-of-the-dot/Kotd-title-matches')
+page = agent.get(
+                  'https://genius.com/albums/King-of-the-dot/Kotd-title-matches'
+                )
+arr = []
+ page.links.each do |link|
+   if link.text.include?('(Title Match)')
+     arr << link
+   end
+end
