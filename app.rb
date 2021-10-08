f201d00ee75468efbb751f71d70acd24da67ac6d
Vseotets <metalworldowner@gmail.com>
2021-10-08 13:13:47 +0300

    add lyrics array with hashes of battle's texts

--- app.rb
+++ app.rb
@@ -6,9 +6,21 @@ agent = Mechanize.new
 page = agent.get(
                   'https://genius.com/albums/King-of-the-dot/Kotd-title-matches'
                 )
-arr = []
+links = []
  page.links.each do |link|
    if link.text.include?('(Title Match)')
      arr << link
    end
 end
+
+lyrics = []
+links.each do |link|
+  battle = link.click
+  title = battle.search('h2').text.split(battle.search('h2 a').text)
+  lyric = battle.search('.lyrics').text
+  lyrics.push(title: title.last.strip,
+              lyrics: lyric)
+
+
+end
+
