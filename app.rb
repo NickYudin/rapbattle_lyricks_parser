c4ab7db2afdff9260166db0d4c78f207e6326cb1
Vseotets <metalworldowner@gmail.com>
2021-10-13 16:33:33 +0300

    more refactoring
    for Refactoring God

--- app.rb
+++ app.rb
@@ -7,46 +7,43 @@ class Parser
   START_PAGE  = 'https://genius.com/albums/King-of-the-dot/Kotd-title-matches'.freeze
 
   def initialize
-    @agent = Mechanize.new
-    @page = @agent.get(START_PAGE)
-    get_data
+    @page = Mechanize.new.get(START_PAGE)
   end
  
-
-  def get_data
-    @data = []
-    get_links
-    parse_battles
-    puts @data
+  def call
+    battles_data
   end
 
-  def get_links
-    @links = @page.links.filter_map do |link|
-      link if link.text.include?('(Title Match)')
-    end
+  def links
+    @page.links.filter_map { |link| link if link.text.include?('(Title Match)') } 
   end
 
-  def parse_battles
-    @links.each do |link|
-     
-      some = Battle.new (link)
-      @data << some 
-    end 
+  def battles_data
+    links.map { |link| Battle.new(link)} 
   end  
+
 end
 
 class Battle 
 
   def initialize (link)
-    parse_url(link)
-    battle = link.click
-    parse_battle(battle)
+    @link =link
+    @battle = link.click
+  end
+
+  def call
+    parse_battle(@battle)
+  end
+
+  def parse_battle (link)
+    parse_title(link)
+    parse_url(@link)
+    parse_battlers(link)
+    parse_texts(link)
     make_hash
-    return @data
   end
 
   def make_hash
-    
     @data = { 
               title: @title,
               link: @url,
@@ -61,13 +58,6 @@ class Battle
     @url = link.href
   end    
 
-  def parse_battle (link)
-    parse_title(link)
-    parse_1st_battler(link)
-    parse_2nd_battler(link)
-    parse_texts(link)
-  end
-
   def parse_title (link)
       @title = link.search('h2.text_label').text.delete_suffix(' (Title Match) Lyrics')
   end
@@ -78,33 +68,31 @@ class Battle
     lyric = link.search('.lyrics').text.strip.split('[Round ')
     lyric.delete_at(0)
     compilate_battlers_text(lyric)
-
   end
 
   def compilate_battlers_text (rounds)
     rounds.each do |round|
       arr = round.split(']')
-      round_title = arr[0]
-      round_text = arr[1]
-      compilation(round_title, round_text)
+      compilation(arr[0], arr[1])
     end    
   end
 
   def compilation (round_title, round_text)
-      if round_title.include?("#{@first_battler}")
-        @first_text << "#{round_text}"
-      else
-        @second_text << "#{round_text}"
-      end
+      if round_title.include?("#{@first_battler}") ? add_to_first(round text) : add_to_second(round text)
   end  
 
-  def parse_1st_battler (link)
+  def add_to_first (round text)
+    @first_text << "#{round_text}"
+  end
+
+  def add_to_second (round text)
+    @second_text << "#{round_text}"
+  end
+
+  def parse_battlers (link)
     battlers = @title.split(' vs. ') 
     @first_battler = battlers[0]
-  end
-    
-  def parse_2nd_battler (link)
-    battlers = @title.split(' vs. ') 
     @second_battler = battlers[1]
   end
+  
 end
